a737cf4431fbd00ab1c8f7f1a5da10893e1d002d
HesionLeadership <pete@hesionleadership.com>
2026-07-13 09:39:55 -0500

    Update feed_diagnostic.py

--- src/feed_diagnostic.py
+++ src/feed_diagnostic.py
@@ -17,14 +17,14 @@ RSS_FEEDS = {
     "MIT Sloan": "https://mitsloan.mit.edu/feed",
     "Fast Company": "https://www.fastcompany.com/latest/rss",
     "Inc": "https://www.inc.com/rss",
-    "SHRM": https://www.shrm.org/rss,
+    "SHRM": "https://www.shrm.org/rss",
     
     # Google News workarounds (for sources that killed their RSS)
     "Reuters via Google": "https://news.google.com/rss/search?q=site:reuters.com+when:3d&hl=en-US&gl=US&ceid=US:en",
     "WSJ via Google": "https://news.google.com/rss/search?q=site:wsj.com+when:3d&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=CEO+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=management+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
-    "https://news.google.com/rss/search?q=leadership+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
+    "Reuters CEO via Google": "https://news.google.com/rss/search?q=CEO+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
+    "Reuters Mgmt via Google": "https://news.google.com/rss/search?q=management+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
+    "Reuters Leadership via Google": "https://news.google.com/rss/search?q=leadership+site:reuters.com+when:7d&hl=en-US&gl=US&ceid=US:en",
     
     # Tech & Culture
     "TechCrunch": "https://techcrunch.com/feed/",
